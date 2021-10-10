c539919bcaf463400a8a326857ab70a2a804ed87
Celina Takeda <takedacarina@gmail.com>
2021-10-10 08:34:56 -0300

    commit

---
+++ tratamentoeexcecoes/identificarerros.js
@@ -0,0 +1,24 @@
+class CustomError extends Error {
+    constructor({ message, data }) {
+        super(message);
+        this.data = data;
+    }
+}
+
+try {
+    const name = 'Celso Henrique';
+
+    const myError = new CustomError({ 
+        message: 'Custom message on custom error', 
+        data: {
+            type: 'Server error'
+        }
+    });
+
+    throw myError;
+} catch (err) {
+    console.log(err);
+    console.log(err.data);
+} finally {
+    console.log('Keep going...');
+}
